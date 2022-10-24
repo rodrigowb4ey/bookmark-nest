ed2102b95049e15fb50c99a9f8b3238d73cdae29
Rodrigo Saraiva <rodrigobezerrasaraiva@gmail.com>
2022-10-24 10:02:27 -0300

    Prisma initial schema + initial db module structure

--- bookmarks-api/src/auth/auth.module.ts
+++ bookmarks-api/src/auth/auth.module.ts
@@ -1,8 +1,10 @@
 import { Module } from "@nestjs/common";
+import { DbModule } from "src/db/db.module";
 import { AuthController } from "./auth.controller";
 import { AuthService } from "./auth.service";
 
 @Module({
+    imports: [DbModule],
     controllers: [AuthController],
     providers: [AuthService],
 
--- bookmarks-api/src/auth/auth.service.ts
+++ bookmarks-api/src/auth/auth.service.ts
@@ -1,7 +1,13 @@
 import { Injectable } from "@nestjs/common";
+import { User, Bookmark } from "@prisma/client";
+import { DbService } from "src/db/db.service";
 
 @Injectable({})
 export class AuthService {
+    constructor(private db:DbService) {
+        
+    }
+
     signUp() {
         return {msg: 'I have signed up'};
     }
