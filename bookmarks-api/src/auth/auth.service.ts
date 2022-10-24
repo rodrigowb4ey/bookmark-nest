be0dafb5d520bdb6aa2672ed5477797103905725
Rodrigo Saraiva <rodrigobezerrasaraiva@gmail.com>
2022-10-24 15:07:12 -0300

    Initial implementation of the signup function

--- bookmarks-api/src/auth/auth.service.ts
+++ bookmarks-api/src/auth/auth.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from "@nestjs/common";
 import { User, Bookmark } from "@prisma/client";
 import { DbService } from "src/db/db.service";
 import { AuthDto } from "./dto";
+import * as argon from "argon2";
 
 @Injectable()
 export class AuthService {
@@ -9,8 +10,19 @@ export class AuthService {
         
     }
 
-    signup(dto: AuthDto) {
-        return {msg: 'I have signed up'};
+    async signup(dto: AuthDto) {
+        const hash = await argon.hash(dto.password);
+
+        const user = await this.db.user.create({
+            data: {
+                email: dto.email,
+                hash,
+            },
+        });
+
+        delete user.hash;
+
+        return user;
     }
 
     signin(dto: AuthDto) {
