315174811e2b297d857c85c033506b0cbdaea090
Rodrigo Saraiva <rodrigobezerrasaraiva@gmail.com>
2022-10-25 14:19:48 -0300

    Sign up logic

--- bookmarks-api/src/auth/auth.service.ts
+++ bookmarks-api/src/auth/auth.service.ts
@@ -1,8 +1,9 @@
-import { Injectable } from "@nestjs/common";
+import { ForbiddenException, Injectable } from "@nestjs/common";
 import { User, Bookmark } from "@prisma/client";
 import { DbService } from "src/db/db.service";
 import { AuthDto } from "./dto";
 import * as argon from "argon2";
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime";
 
 @Injectable()
 export class AuthService {
@@ -13,17 +14,27 @@ export class AuthService {
     async signup(dto: AuthDto) {
         const hash = await argon.hash(dto.password);
 
-        const user = await this.db.user.create({
-            data: {
-                email: dto.email,
-                hash,
-            },
-        });
+        try {
+            const user = await this.db.user.create({
+                data: {
+                    email: dto.email,
+                    hash,
+                },
+            });
+    
+            delete user.hash;
+    
+            return user;
+        } catch (error) {
+            if (error instanceof PrismaClientKnownRequestError) {
+                if (error.code === 'P2002') {
+                    throw new ForbiddenException('Credentials taken')
+                };
+            };
+            throw error;
+        };
 
-        delete user.hash;
-
-        return user;
-    }
+    };
 
     signin(dto: AuthDto) {
         return {msg: 'I have signed in'};
